9cfda561c2d68c3de579a05e351540e0074cec10
Renu Punjabi <rpunjabi12@gmail.com>
2024-05-23 16:17:14 -0700

    Initial commit

---
+++ MarvelTV/MarvelTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelTVApp.swift
+//  MarvelTV
+//
+//  Created by Renu Punjabi on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MarvelTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
