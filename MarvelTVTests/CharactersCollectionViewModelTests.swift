3d38ab92b058659763eebd67098541572d59f3f7
Renu Punjabi <rpunjabi12@gmail.com>
2024-05-25 05:27:08 -0700

    Add unit test for CharacterCollectionViewModel

---
+++ MarvelTVTests/CharactersCollectionViewModelTests.swift
@@ -0,0 +1,67 @@
+//
+//  CharactersCollectionViewModelTests.swift
+//  MarvelTVTests
+//
+//  Created by Renu Punjabi on 5/25/24.
+//
+
+import XCTest
+@testable import MarvelTV
+
+enum FileName: String {
+    case characters, comics
+}
+
+final class CharactersCollectionViewModelTests: XCTestCase {
+    var viewModel: CharactersCollectionViewModel!
+    var mockService: MockCharactersRepository!
+    
+    override func setUpWithError() throws {
+        mockService = MockCharactersRepository(fileName: .characters)
+        viewModel = CharactersCollectionViewModel(charactersRepository: mockService)
+    }
+
+    override func tearDownWithError() throws {
+        mockService = nil
+        viewModel = nil
+    }
+
+    func test_pullCharacters_should_succeed() async throws {
+        viewModel.pullAllCharacters()
+        
+        XCTAssertEqual(viewModel.asyncState, .loading)
+        
+        let exp = XCTestExpectation(description: "characters api call should succeed")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+            XCTAssertEqual(self.viewModel.asyncState, .loaded)
+            XCTAssertEqual(self.viewModel.characters.first!.name, "3-D Man")
+            exp.fulfill()
+        }
+        
+        await fulfillment(of: [exp], timeout: 5)
+    }
+
+}
+
+class MockCharactersRepository: CharactersRepository {
+    
+    let fileName: FileName
+    
+    init(fileName: FileName) {
+        self.fileName = fileName
+    }
+    
+    private func load(_ fileName: String) -> URL? {
+        return Bundle(for: type(of: self)).url(forResource: fileName, withExtension: "json")
+    }
+    
+    func fetchAllCharacters() async throws -> [MarvelTV.Character] {
+        guard let url = load(fileName.rawValue) else { throw URLError(.badURL) }
+        let data = try! Data(contentsOf: url)
+        let result = try JSONDecoder().decode(DataWrapper<Character>.self, from: data)
+        return result.data.results
+    }
+    
+    
+}
